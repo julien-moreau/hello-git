1d7dca70c4a082c486a9f9e06500de240652807a
julien-moreau <moreau-mathis@intechinfo.fr>
2022-09-22 09:59:25 +0200

    add multiply function

--- index.js
+++ index.js
@@ -2,5 +2,12 @@ function sum(a, b) {
     return a + b;
 }
 
+function multiply(a, b) {
+    return a * b;
+}
+
 console.log(sum(1, 1));
 console.log(sum(2, 2));
+
+console.log(multiply(1, 1));
+console.log(multiply(2, 2));
